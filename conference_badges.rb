b49fd4e857f0fa3b94ebc3b2fe7e71e686bfb5a4
Ricky Liu <rorix12321@gmail.com>
2018-08-12 09:54:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,10 @@ def badge_maker(name)
   badge = "Hello, my name is #{name}."
   return badge
 end
+
+def batch_badge_creator(names)
+  array1 = []
+  names.each do |x|
+    array1 << "x"
+    return array1
+  end
